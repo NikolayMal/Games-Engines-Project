a56ac1aa80cb35ef009e8c952963a6f8dd8bee09
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2021-11-23 15:33:42 +0000

    Project Start
    Project Created, Generates Cubes(instead of buildings) on top of a plane.

---
+++ Cities of Sound/Assets/Scripts/gridTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridTest : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(5, 5, 10f);
+    }
+}
