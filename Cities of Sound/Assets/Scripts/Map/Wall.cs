6a3f931c01e12e2341d90da0d18ca59d0d09a18c
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2021-12-14 17:03:55 +0000

    Quad Sides
    Box now completely generates, with 4 sides

--- Cities of Sound/Assets/Scripts/Map/Wall.cs
+++ Cities of Sound/Assets/Scripts/Map/Wall.cs
@@ -14,18 +14,43 @@ public class Wall : MonoBehaviour
     void Start()
     {
         int gap = 10;
-        for (int row = 0; row < 20; row++)
+
+        // Loops through to generate all 4 sides 
+        for (int i = 0; i < 4; i++)
         {
-            for (int col = 0; col < SizeY + gap; col++)
+            // Generate Row and Columns of Wall
+            for (int row = 0; row < 20; row++)
             {
-                GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                wall.transform.position = transform.TransformPoint(new Vector3(col - (gap / 2) - 1, 7f + row, 0));
-                wall.transform.rotation = transform.rotation;
-                wall.GetComponent<Renderer>().material.color = Color.red;
-                // wall.AddComponent<Rigidbody>();
-                wall.transform.parent = this.transform;
-                // wall.GetComponent<Rigidbody>().mass = 1000;
-                wall.layer = 1;
+                for (int col = 0; col < SizeY + gap; col++)
+                {
+                    GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    if (i == 0)
+                    {
+                        wall.transform.position = transform.TransformPoint(new Vector3(col - (gap / 2) - 1, 7f + row, 0));
+
+                    }
+                    if (i == 1)
+                    {   // z + ( to build in opposite direction of  i === 0)
+                        wall.transform.position = transform.TransformPoint(new Vector3(col - (gap / 2) - 1, 7f + row, SizeX + gap));
+
+                    }
+                    if (i == 2)
+                    {
+                        wall.transform.position = transform.TransformPoint(new Vector3(-gap + 3, 7f + row, col + 1));
+
+                    }
+                    if (i == 3)
+                    {
+                        wall.transform.position = transform.TransformPoint(new Vector3(gap + SizeX - 6, 7f + row, col + 1));
+                    }
+
+                    wall.transform.rotation = transform.rotation;
+                    wall.GetComponent<Renderer>().material.color = Color.red;
+                    // wall.AddComponent<Rigidbody>();
+                    wall.transform.parent = this.transform;
+                    // wall.GetComponent<Rigidbody>().mass = 1000;
+                    wall.layer = 1;
+                }
             }
         }
     }
