49661bce261b868fbc4fbb5734a3ccb77a0462d2
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2021-12-14 20:22:43 +0000

    Tank Shooting
    Tank now shoots appropriately with base weapon, on all axis'

--- Cities of Sound/Assets/Scripts/Player/Weapon1.cs
+++ Cities of Sound/Assets/Scripts/Player/Weapon1.cs
@@ -16,11 +16,13 @@ public class Weapon1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transform.rotation = Quaternion.Euler(0f, 0f, offset);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // Wait a a little until weapon starts ( so player can center mouse in time )
         if (timeCounter < startDelay)
         {
@@ -30,7 +32,7 @@ public class Weapon1 : MonoBehaviour
 
         if (timeBetweenShot <= 0)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKey(KeyCode.Space))
             {
                 Instantiate(projectile, ShotPoint.position, transform.rotation);
                 timeBetweenShot = starttimeBetweenShot;
